0a527cdd8ab1fa74a58e53f5c48dbe8d54c4b77a
makkurotsukushitsudayuya-wq <makkurotsukushi.tsudayuya@gmail.com>
2026-08-30 23:53:03 +0900

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 // ここだけ環境に合わせて書き換えれば動きます。
 window.APP_CONFIG = {
   // 出勤確認アプリのGAS Webアプリ デプロイURL(/exec まで)
-  GAS_WEB_APP_URL: 'https://script.google.com/macros/s/XXXXXXXXXXXXXXXX/exec',
+  GAS_WEB_APP_URL: 'https://script.google.com/macros/s/AKfycbyzfVME8-9XEc0-61JgmkzRZA9zx8VrarFR_oqb5D4ORsXUi9E_7txNUy-as_hYeg-t/exec',
   // Code.gs の SHARED_SECRET スクリプトプロパティと同じ値にする
-  SHARED_SECRET: 'PUT_THE_SAME_SHARED_SECRET_HERE',
+  SHARED_SECRET: 'Z6GSrjrXXshm2VYk5cBf1vB5JJSz7V1g',
   POSITIONS: ['ホール', 'サブ', '麺場'],
 };
